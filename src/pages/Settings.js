5824bbd5f0bcccd7483c7aa86a4437762bccd5f2
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-04-02 16:55:03 +0530

    settings page data fetch from graphql

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -3,16 +3,21 @@ import { useHistory } from "react-router-dom";
 import Tabs from "../components/Nav/Tabs";
 import { BrandHeader } from "../components/Nav/Headers";
 import { UserContext } from "../context/UserProvider";
+import { useQuery } from "@apollo/client";
+import { GET_BUYER } from "../graphql/queries";
 
 export default function Settings() {
   const user = useContext(UserContext);
+  const { loading, error, data } = useQuery(GET_BUYER);
   const history = useHistory();
+  if (loading) return null;
+  if (error) console.error(error);
   return (
     <>
       <Tabs />
       <BrandHeader />
       {user.userId
-        ? `Welcome, ${user.userId} 👋`
+        ? `Welcome, ${!data ? user.userId : data.buyer.name} 👋`
         : history.push("/login", {
             ref: "/settings",
           })}
