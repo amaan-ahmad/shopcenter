5c1f15ccc9b9e2652f6958a8e230997ef12a46b7
Amaan Ahmad <amaanahmad582001@gmail.com>
2021-05-24 18:46:20 +0530

    PaperCard added

---
+++ src/components/Styles/PaperCard.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const PaperCard = styled.div`
+  width: -webkit-fill-available;
+  padding: 0.6em;
+  border-radius: 12px;
+  background-color: white;
+  box-shadow: 0 5px 15px -10px grey;
+  margin: 1em 0;
+  font-size: 1rem;
+`;
